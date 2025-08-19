21ddcbb0ff91a5e5a558f808c4738b6dbc121dca
Александр <alexgog77777@gmail.com>
2025-08-19 07:59:39 +0500

    work

---
+++ hw_django_orm/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Product, Category
+
+
+@admin.register(Product)
+class Admin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')
+    search_fields = ('name', 'description')
+
+
+@admin.register(Category)
+class Admin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name', 'description')
---
+++ hw_django_orm/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'hw_django_orm'
+
+urlpatterns = [
+    #path('home/', views.home, name='home'),
+    #path('contacts/', views.contacts, name='contacts')
+]
