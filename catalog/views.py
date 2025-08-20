612acc36a0b60f824891ff01e29a88540afb98fc
Александр <alexgog77777@gmail.com>
2025-08-21 04:39:55 +0500

    Unite

---
+++ catalog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        return HttpResponse (f'Спасибо за отправку формы {name}')
+    return render(request, 'catalog/contacts.html')
--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'catalog',
     'hw_django_orm',
 ]
 
