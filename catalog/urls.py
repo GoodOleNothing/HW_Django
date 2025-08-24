3c54a35aa57b92a689ed1fe636025422853eed94
Александр <alexgog77777@gmail.com>
2025-08-25 00:47:33 +0500

    Дз шаблонизация

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,8 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts')
+    path('contacts/', views.contacts, name='contacts'),
+    path('product_catalog/', views.product_catalog, name='product_catalog'),
+    path('product/<int:id>', views.product, name='product'),
+    path('add_product/', views.add_product, name='add_product'),
 ]
