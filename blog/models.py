553a29312778e5a6811c56a41cb6f0f69043a6d9
Александр <alexgog77777@gmail.com>
2025-08-30 00:27:20 +0500

    Дз CBV

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=150, verbose_name='Заголовок')
-    text = models.TextField(null=True, blank=True, verbose_name='Описание')
+    content = models.TextField(null=True, blank=True, verbose_name='Описание')
     image = models.ImageField(blank=True, default=None, upload_to='blog_image/', verbose_name='Изображение')
     active = models.BooleanField(default=True, verbose_name='Активно')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,50 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView,TemplateView
+from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.edit import UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.core.mail import send_mail
 from .models import Blog
 
 # Create your views here.
 
 
-class BlogView(ListView):
+class BlogListView(ListView):
     model = Blog
+
+    def get_queryset(self):
+        return Blog.objects.filter(active=True)
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get_object(self, queryset=None):
+        count = super().get_object()
+        count.views_count += 1
+        count.save()
+        if count.views_count == 10:
+            send_mail(
+                subject="100 просмотров",
+                message=f"Запись '{count.title}' достигла 100 просмотров",
+                recipient_list=["email@example.com"],
+            )
+        return count
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ['title', 'content', 'image', 'active']
+    success_url = reverse_lazy('blog:blog_list')
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ['title', 'content', 'image', 'active']
+
+    def get_success_url(self):
+        return reverse("blog:blog_detail", kwargs={"pk": self.object.pk})
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog:blog_list')
