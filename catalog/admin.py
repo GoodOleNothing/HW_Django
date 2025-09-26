190ad6fcef4cbe246e37c8fe53587aa551445114
Александр <alexgog77777@gmail.com>
2025-09-27 01:42:18 +0500

    Дз Права доступа

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,11 +5,12 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Product, Category
+from users.models import User
 
 
 @admin.register(Product)
 class Admin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category')
+    list_display = ('id', 'name', 'price', 'category', 'status', 'owner')
     search_fields = ('name', 'description')
     list_filter = ('category',)
 
@@ -18,3 +19,8 @@ class Admin(admin.ModelAdmin):
 class Admin(admin.ModelAdmin):
     list_display = ('id', 'name')
     search_fields = ('name', 'description')
+
+
+@admin.register(User)
+class Admin(admin.ModelAdmin):
+    list_display = ('email', 'phone', 'avatar', 'country')
--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 # Create your models here.
@@ -6,10 +7,14 @@ class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Имя')
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
     image = models.ImageField(blank=True, default=None, upload_to='product_image/', verbose_name='Изображение')
-    category = models.ForeignKey(to='Category',on_delete=models.CASCADE, related_name='Кат')
+    category = models.ForeignKey(to='Category', on_delete=models.CASCADE, related_name='Кат')
     price = models.IntegerField(verbose_name='Цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    status = models.BooleanField(default=True, verbose_name='Активен')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='Владелец',
+                              null=True, blank=True)
 
     def __str__(self):
         return f'{self.name}, {self.category}'
@@ -18,6 +23,11 @@ class Product(models.Model):
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
         ordering = ['id']
+        permissions = (
+            ("can_unpublish_product", "Can unpublish product"),
+            ("can_change_product_price", "Can change product price"),
+            ("can_change_product_description", "Can change product description"),
+        )
 
 
 class Category(models.Model):
@@ -31,3 +41,4 @@ class Category(models.Model):
         verbose_name = 'Категоря'
         verbose_name_plural = 'Категории'
         ordering = ['id']
+
